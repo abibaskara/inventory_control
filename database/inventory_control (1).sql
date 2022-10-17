517a7e4ad33c5ac2d6e17326a4ada10560db80b6
abibaskara <abibaskara211@gmail.com>
2022-10-17 09:46:28 +0700

    update-database

--- database/inventory_control (1).sql
+++ database/inventory_control (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 21 Jul 2022 pada 04.13
--- Versi server: 5.7.33
--- Versi PHP: 7.4.19
+-- Generation Time: Oct 17, 2022 at 02:39 AM
+-- Server version: 5.7.33
+-- PHP Version: 7.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
 CREATE TABLE `barang` (
@@ -38,7 +38,7 @@ CREATE TABLE `barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang`
+-- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`id_barang`, `id_kategori`, `id_supplier`, `nama_barang`, `satuan`, `berat_barang`, `keterangan`) VALUES
@@ -49,7 +49,7 @@ INSERT INTO `barang` (`id_barang`, `id_kategori`, `id_supplier`, `nama_barang`, 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_keluar`
+-- Table structure for table `barang_keluar`
 --
 
 CREATE TABLE `barang_keluar` (
@@ -64,30 +64,17 @@ CREATE TABLE `barang_keluar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang_keluar`
+-- Dumping data for table `barang_keluar`
 --
 
 INSERT INTO `barang_keluar` (`id_barang_keluar`, `tgl_keluar`, `id_permintaan`, `id_departement`, `id_barang`, `qty_keluar`, `qty_diserahkan`, `batch_number`) VALUES
-(1, '2022-07-15', 1, 12, 1, 0, 1, '001_001'),
-(2, '2022-07-15', 1, 12, 2, 0, 1, '002_001'),
-(3, '2022-07-15', 1, 12, 1, 0, 1, '001_001'),
-(4, '2022-07-15', 2, 12, 1, 0, 1, '001_001'),
-(5, '2022-07-15', 2, 12, 2, 0, 1, '002_001'),
-(6, '2022-07-15', 2, 12, 1, 0, 1, '001_001'),
-(7, '2022-07-15', 7, 12, 1, 0, 1, '001_001'),
-(8, '2022-07-15', 7, 12, 1, 0, 1, '001_001'),
-(9, '2022-07-15', 10, 12, 1, 0, 1, '001_001'),
-(10, '2022-07-21', 12, 12, 1, 0, 1, '001_003'),
-(11, '2022-07-21', 13, 12, 1, 0, 1, '001_003'),
-(12, '2022-07-21', 13, 12, 1, 0, 1, '001_003'),
-(13, '2022-07-21', 10, 12, 1, 0, 1, '001_003'),
-(14, '2022-07-21', 13, 12, 1, 0, 1, '001_001'),
-(15, '2022-07-21', 11, 12, 1, 0, 1, '001_001');
+(1, '2022-08-24', 1, 12, 1, 0, 1, '001_001'),
+(2, '2022-08-24', 1, 12, 2, 0, 1, '002_002');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang_masuk`
+-- Table structure for table `barang_masuk`
 --
 
 CREATE TABLE `barang_masuk` (
@@ -102,17 +89,18 @@ CREATE TABLE `barang_masuk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `barang_masuk`
+-- Dumping data for table `barang_masuk`
 --
 
 INSERT INTO `barang_masuk` (`id_barang_masuk`, `batch_number`, `tgl_datang`, `id_supplier`, `id_barang`, `satuan`, `qty_masuk`, `exp`) VALUES
-(18, '002_001', '2022-07-20', 1, 2, 'Pcs', 2, '2022-07-31'),
-(19, '001_001', '2022-07-21', 1, 1, 'Pcs', 2, '2022-07-31');
+(1, '001_001', '2022-08-24', 1, 1, 'Pcs', 1, '2022-08-31'),
+(2, '002_001', '2022-08-24', 1, 2, 'Pcs', 4, '2022-08-30'),
+(4, '002_002', '2022-08-24', 1, 2, 'Pcs', 2, '2022-08-26');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `batch_number`
+-- Table structure for table `batch_number`
 --
 
 CREATE TABLE `batch_number` (
@@ -121,10 +109,17 @@ CREATE TABLE `batch_number` (
   `id_barang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `batch_number`
+--
+
+INSERT INTO `batch_number` (`id_batch_number`, `batch_number`, `id_barang`) VALUES
+(1, '001_002', 1);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `departement`
+-- Table structure for table `departement`
 --
 
 CREATE TABLE `departement` (
@@ -133,7 +128,7 @@ CREATE TABLE `departement` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `departement`
+-- Dumping data for table `departement`
 --
 
 INSERT INTO `departement` (`id_departement`, `nama_departement`) VALUES
@@ -144,7 +139,7 @@ INSERT INTO `departement` (`id_departement`, `nama_departement`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `detail_permintaan`
+-- Table structure for table `detail_permintaan`
 --
 
 CREATE TABLE `detail_permintaan` (
@@ -157,30 +152,19 @@ CREATE TABLE `detail_permintaan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `detail_permintaan`
+-- Dumping data for table `detail_permintaan`
 --
 
 INSERT INTO `detail_permintaan` (`id_detail_permintaan`, `id_permintaan`, `id_barang`, `qty_permintaan`, `qty_keluar_permintaan`, `ket_permintaan`) VALUES
-(1, 1, 1, 2, 2, 'asdsad'),
-(2, 1, 2, 1, 1, 'asdasd'),
-(3, 2, 1, 2, 2, 'adsada'),
-(4, 2, 2, 1, 1, 'asdsadsa'),
-(5, 2, 3, 1, 0, 'asdaad'),
-(8, 3, 1, 1, 1, 'sadsad'),
-(9, 3, 1, 1, 1, 'adasd'),
-(10, 8, 1, 2, 2, 'asdas'),
-(11, 11, 1, 2, 1, 'adsada'),
-(12, 12, 1, 1, 1, 'asdsad'),
-(13, 13, 1, 3, 3, 'asdsad'),
-(14, 14, 1, 1, 0, 'asdasda'),
-(15, 15, 1, 1, 0, 'asdsad'),
-(16, 16, 1, 1, 0, 'sadsad'),
-(17, 16, 2, 1, 0, 'asdasd');
+(1, 1, 1, 1, 1, 'sadass'),
+(2, 1, 2, 1, 1, 'asdsad'),
+(3, 2, 1, 1, 0, 'asdsa'),
+(4, 2, 2, 1, 0, 'asdsad');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `detail_po`
+-- Table structure for table `detail_po`
 --
 
 CREATE TABLE `detail_po` (
@@ -192,7 +176,7 @@ CREATE TABLE `detail_po` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `detail_po`
+-- Dumping data for table `detail_po`
 --
 
 INSERT INTO `detail_po` (`id_detail_po`, `id_po`, `id_barang`, `qty_po`, `ket_po`) VALUES
@@ -201,12 +185,16 @@ INSERT INTO `detail_po` (`id_detail_po`, `id_po`, `id_barang`, `qty_po`, `ket_po
 (3, 3, 3, 3, 'sadsada'),
 (4, 3, 2, 2, 'sadsadsa'),
 (5, 4, 2, 4, 'asdsad'),
-(6, 4, 1, 2, 'asdsada');
+(6, 4, 1, 2, 'asdsada'),
+(7, 5, 0, 0, ''),
+(8, 9, 1, 1, 'asdsad'),
+(9, 10, 1, 1, 'asdsad'),
+(10, 10, 2, 2, 'asdsad');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kategori_barang`
+-- Table structure for table `kategori_barang`
 --
 
 CREATE TABLE `kategori_barang` (
@@ -215,7 +203,7 @@ CREATE TABLE `kategori_barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kategori_barang`
+-- Dumping data for table `kategori_barang`
 --
 
 INSERT INTO `kategori_barang` (`id_kategori`, `nama_kategori`) VALUES
@@ -225,7 +213,7 @@ INSERT INTO `kategori_barang` (`id_kategori`, `nama_kategori`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `permintaan_barang`
+-- Table structure for table `permintaan_barang`
 --
 
 CREATE TABLE `permintaan_barang` (
@@ -238,25 +226,17 @@ CREATE TABLE `permintaan_barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `permintaan_barang`
+-- Dumping data for table `permintaan_barang`
 --
 
 INSERT INTO `permintaan_barang` (`id_permintaan`, `id_departement`, `id_detail_permintaan`, `tgl_permintaan`, `status_permintaan`, `view_permintaan`) VALUES
-(1, 12, 1, '2022-07-15', 'Selesai', 0),
-(2, 12, 2, '2022-07-15', 'Selesai', 0),
-(7, 12, 3, '2022-07-15', 'Selesai', 0),
-(10, 12, 8, '2022-07-15', 'Selesai', 0),
-(11, 12, 11, '2022-07-18', 'Proses', 0),
-(12, 12, 12, '2022-07-18', 'Selesai', 0),
-(13, 12, 13, '2022-07-18', 'Selesai', 0),
-(14, 12, 14, '2022-07-18', 'Waiting', 0),
-(15, 12, 15, '2022-07-18', 'Waiting', 0),
-(19, 12, 16, '2022-07-21', 'Waiting', 0);
+(1, 12, 1, '2022-08-24', 'Selesai', 0),
+(2, 12, 2, '2022-08-24', 'Waiting', 0);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `purchase_order`
+-- Table structure for table `purchase_order`
 --
 
 CREATE TABLE `purchase_order` (
@@ -267,19 +247,25 @@ CREATE TABLE `purchase_order` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `purchase_order`
+-- Dumping data for table `purchase_order`
 --
 
 INSERT INTO `purchase_order` (`id_po`, `id_user`, `tgl_po`, `id_detail_po`) VALUES
 (1, 1, '2022-07-18', 1),
 (2, 1, '2022-07-18', 2),
 (3, 1, '2022-07-18', 3),
-(4, 1, '2022-07-18', 4);
+(4, 1, '2022-07-18', 4),
+(5, 1, '2022-08-24', 5),
+(6, 1, '2022-08-24', 6),
+(7, 1, '2022-08-24', 7),
+(8, 1, '2022-08-24', 8),
+(9, 1, '2022-08-24', 9),
+(10, 1, '2022-08-24', 10);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `stok_barang`
+-- Table structure for table `stok_barang`
 --
 
 CREATE TABLE `stok_barang` (
@@ -291,18 +277,18 @@ CREATE TABLE `stok_barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `stok_barang`
+-- Dumping data for table `stok_barang`
 --
 
 INSERT INTO `stok_barang` (`id_stok_barang`, `id_barang`, `qty_stok`, `created_at`, `updated_at`) VALUES
-(1, 1, 2, '2022-07-14', '2022-07-21'),
-(2, 2, 2, '2022-07-14', '2022-07-20'),
+(1, 1, 1, '2022-07-14', '2022-08-24'),
+(2, 2, 6, '2022-07-14', '2022-08-24'),
 (3, 3, 0, '2022-07-15', '2022-07-15');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `supplier`
+-- Table structure for table `supplier`
 --
 
 CREATE TABLE `supplier` (
@@ -316,16 +302,16 @@ CREATE TABLE `supplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `supplier`
+-- Dumping data for table `supplier`
 --
 
 INSERT INTO `supplier` (`id_supplier`, `nama_supplier`, `alamat`, `no_telp_supplier`, `email_supplier`, `nama_pic_supplier`, `no_telp_pic_supplier`) VALUES
-(1, 'Abi Baskara A', 'Kober', '(+62) 8229-9915-127', 'abibaskara211@gmail.com', 'Irfan', '(+62) 8123-4567-890');
+(1, 'Abi Baskara A', 'kober', '(+62) 8229-9915-127', 'abibaskara211@gmail.com', 'Irfan', '(+62) 8123-4567-890');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -342,177 +328,178 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `id_departement`, `nama_user`, `jk`, `no_telp`, `email`, `password`, `role`, `foto`, `is_active`) VALUES
 (1, 12, 'Abi Baskara Atthallah', 'Pria', '082299915127', 'admin@gmail.com', '$2y$10$GqoVemjyWxmEcORrxgdiEu3.nnHCJJGSKSaJjDanSKEyTdm7riUqi', 'Inventory', '1.png', 1),
 (2, 12, 'Produksi', 'Pria', '(+62) 8229-9915-127', 'produksi@gmail.com', '$2y$10$Xj07yJxs4BGsBrFObU4q4eZxnL2J0Gea6iibmvOBWoOAU4mXncYFO', 'Produksi', 'icon_survey_(4).png', 1),
-(4, 12, 'Manager', 'Pria', '(+62) 8229-9915-127', 'manager@gmail.com', '$2y$10$N271c1jIoFJrwEyFK0QJYuma209../9zj6aEB4hZg.I9zdR9bAixi', 'Manager', 'icon_survey_(3).png', 1);
+(4, 12, 'Manager', 'Pria', '(+62) 8229-9915-127', 'manager@gmail.com', '$2y$10$N271c1jIoFJrwEyFK0QJYuma209../9zj6aEB4hZg.I9zdR9bAixi', 'Manager', 'icon_survey_(3).png', 1),
+(7, 12, 'test', 'Pria', '(+62) 8229-9915-127', 'test@gmail.com', '$2y$10$rawOFVL28iL8UyvnIWO/Vu1vCUoXX94OxfiNog0EelDtPOFQpVVqe', 'Inventory', 'default.png', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `barang`
+-- Indexes for table `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
--- Indeks untuk tabel `barang_keluar`
+-- Indexes for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
   ADD PRIMARY KEY (`id_barang_keluar`);
 
 --
--- Indeks untuk tabel `barang_masuk`
+-- Indexes for table `barang_masuk`
 --
 ALTER TABLE `barang_masuk`
   ADD PRIMARY KEY (`id_barang_masuk`);
 
 --
--- Indeks untuk tabel `batch_number`
+-- Indexes for table `batch_number`
 --
 ALTER TABLE `batch_number`
   ADD PRIMARY KEY (`id_batch_number`);
 
 --
--- Indeks untuk tabel `departement`
+-- Indexes for table `departement`
 --
 ALTER TABLE `departement`
   ADD PRIMARY KEY (`id_departement`);
 
 --
--- Indeks untuk tabel `detail_permintaan`
+-- Indexes for table `detail_permintaan`
 --
 ALTER TABLE `detail_permintaan`
   ADD PRIMARY KEY (`id_detail_permintaan`);
 
 --
--- Indeks untuk tabel `detail_po`
+-- Indexes for table `detail_po`
 --
 ALTER TABLE `detail_po`
   ADD PRIMARY KEY (`id_detail_po`);
 
 --
--- Indeks untuk tabel `kategori_barang`
+-- Indexes for table `kategori_barang`
 --
 ALTER TABLE `kategori_barang`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indeks untuk tabel `permintaan_barang`
+-- Indexes for table `permintaan_barang`
 --
 ALTER TABLE `permintaan_barang`
   ADD PRIMARY KEY (`id_permintaan`);
 
 --
--- Indeks untuk tabel `purchase_order`
+-- Indexes for table `purchase_order`
 --
 ALTER TABLE `purchase_order`
   ADD PRIMARY KEY (`id_po`);
 
 --
--- Indeks untuk tabel `stok_barang`
+-- Indexes for table `stok_barang`
 --
 ALTER TABLE `stok_barang`
   ADD PRIMARY KEY (`id_stok_barang`);
 
 --
--- Indeks untuk tabel `supplier`
+-- Indexes for table `supplier`
 --
 ALTER TABLE `supplier`
   ADD PRIMARY KEY (`id_supplier`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `barang`
+-- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
   MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `barang_keluar`
+-- AUTO_INCREMENT for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
-  MODIFY `id_barang_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_barang_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `barang_masuk`
+-- AUTO_INCREMENT for table `barang_masuk`
 --
 ALTER TABLE `barang_masuk`
-  MODIFY `id_barang_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_barang_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `batch_number`
+-- AUTO_INCREMENT for table `batch_number`
 --
 ALTER TABLE `batch_number`
-  MODIFY `id_batch_number` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_batch_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `departement`
+-- AUTO_INCREMENT for table `departement`
 --
 ALTER TABLE `departement`
   MODIFY `id_departement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT untuk tabel `detail_permintaan`
+-- AUTO_INCREMENT for table `detail_permintaan`
 --
 ALTER TABLE `detail_permintaan`
-  MODIFY `id_detail_permintaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_detail_permintaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `detail_po`
+-- AUTO_INCREMENT for table `detail_po`
 --
 ALTER TABLE `detail_po`
-  MODIFY `id_detail_po` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_detail_po` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `kategori_barang`
+-- AUTO_INCREMENT for table `kategori_barang`
 --
 ALTER TABLE `kategori_barang`
   MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `permintaan_barang`
+-- AUTO_INCREMENT for table `permintaan_barang`
 --
 ALTER TABLE `permintaan_barang`
-  MODIFY `id_permintaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_permintaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `purchase_order`
+-- AUTO_INCREMENT for table `purchase_order`
 --
 ALTER TABLE `purchase_order`
-  MODIFY `id_po` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_po` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `stok_barang`
+-- AUTO_INCREMENT for table `stok_barang`
 --
 ALTER TABLE `stok_barang`
   MODIFY `id_stok_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `supplier`
+-- AUTO_INCREMENT for table `supplier`
 --
 ALTER TABLE `supplier`
   MODIFY `id_supplier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
